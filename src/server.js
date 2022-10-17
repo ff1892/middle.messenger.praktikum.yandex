5303d857fe3cf13364c15c7592a76efc5db63f50
ff1892 <hitrulein@gmail.com>
2022-10-17 21:28:50 +0300

    Fix building issues

--- src/server.js
+++ src/server.js
@@ -1,11 +1,15 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const PORT = 3000;
 
-app.use(express.static(`${__dirname}`));
+app.use(express.static('./dist/'));
 
-app.listen(PORT, () => {
-  console.log('Server is running...');
-  console.log(`${__dirname}`);
+app.get('*', function (_request, response) {
+    response.sendFile(path.resolve('./dist/index.html'));
+});
+
+app.listen(PORT, function () {
+    console.log(`App running on port ${PORT}!`);
 });
