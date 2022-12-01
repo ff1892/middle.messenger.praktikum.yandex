280b726d8b63de68649f4ee788add204528fb13e
ff1892 <hitrulein@gmail.com>
2022-12-01 13:55:30 +0300

    Add http test and docker file

--- test/test.js
+++ test/test.js
@@ -2,8 +2,10 @@ const jsdom = require('jsdom');
 
 const { JSDOM } = jsdom;
 
-const { window } = new JSDOM('<div id="root"></div>', { url: 'http://localhost' });
+const { window } = new JSDOM('<div id="root"></div>', { url: 'http://localhost:3000' });
 const { document } = window;
 
 global.window = window;
 global.document = document;
+// eslint-disable-next-line no-undef
+XMLHttpRequest = window.XMLHttpRequest;
